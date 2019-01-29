07a23aca9cea3241f2f0eb76fbe5b98fa44ada39
LiHailun <hailun183@163.com>
2019-01-29 19:42:15 +0800

    创建Vue-cli

---
+++ src/views/NotFound.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="NotFound">
+    <h1>未找到</h1>
+  </div>
+</template>
---
+++ vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    //Webpack开发服务器的设置
+    devServer: {
+        host: '127.0.0.1',
+        port: 8091
+    }
+}
+
+
+
+
